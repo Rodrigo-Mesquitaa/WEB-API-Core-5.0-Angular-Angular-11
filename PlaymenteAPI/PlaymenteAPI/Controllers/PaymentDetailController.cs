305e4f245d994e309c9a799a545f932cfa51face
unknown <rodrigo09.mesquita@gmail.com>
2021-04-12 16:18:55 -0300

    Projeto web API com Core 5.0 e Angular 11.

---
+++ PlaymenteAPI/PlaymenteAPI/Controllers/PaymentDetailController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PlaymenteAPI.Models;
+
+
+namespace PlaymenteAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentDetailController : ControllerBase
+    {
+        public PaymentDetailController(PaymentDetailContext context)
+        {
+            _context = context;
+        }
+
+        //GET: api/PaymentDetail
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<PaymentDetail>>> GetPaymentDetails()
+        {
+            return await _context.PaymenteDetails.ToListAsync();
+        }
+
+        //GET api/PaymentDetail/5
+        [HttpGet]
+        public async Task<ActionResult<PaymenteDetail>> GetPaymentDetail(int id)
+        {
+            var paymentDetail = await _context.PaymentDetails.FindAsync(id);
+
+            if (paymentDetail == null)
+            {
+                return NotFound();
+            }
+
+            return paymentDetail;
+        }
+
+        //PUT: api/PaymentDetails/5
+        //To protect from overposting attacks, see  https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPaymentDetail(int id, PaymenteDetail paymenteDetail)
+        {
+            if (id != paymenteDetail.PaymentDetailId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(paymenteDetail).State = EntityState.Modified;
+
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaymentDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        //POST: api/PaymentDetail
+        //To protect from overposting attacks, see  https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PaymentDetail>> PostPaymentDetail(PaymentDetail paymentDetail)
+        {
+            _context.PaymentDetails.Add(paymentDetail);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPaymentDetail", new { id = paymentDetail.PaymentDetailId }, paymentDetail);
+
+        }
+
+        //DELETE: api/PaymentDetail/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePaymentDetail(int id)
+        {
+            var paymentDetail = await _context.PaymentDetails.FindAsync(id);
+            if (paymentDetail == null)
+            {
+                return NotFound();
+            }
+
+            _context.PaymentDetails.Remove(paymentDetail);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PaymentDetailExists(int id)
+        {
+            return _context.PaymentDetails.Any(e => e.PaymentDetailId == id);
+        }
+    }
+}
